684dd6b3a52c5169888251d20c43f5461f738ae6
Josias Ativon <josiasativon@gmail.com>
2023-07-18 10:51:10 +0100

    add python file

---
+++ helloWord.py
@@ -0,0 +1,10 @@
+# Taking user name
+user_name = input("What is your name:? ")
+print(user_name)
+# Taking in user age
+user_age = input("What is your age:? ")
+print(user_age)
+
+# Printing a hello world on a new line
+print("Hello World!")
+ 
